65a93cd91231fbd651b2f3276d6c21f1c8685eec
pawelwojkiewicz <pwo@crehler.com>
2019-10-17 09:07:13 +0200

    added i18n

--- src/main.js
+++ src/main.js
@@ -10,20 +10,44 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUser, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+Vue.use(VueResource, PasswordHash, VueI18n);
+
 
 library.add(faUser, faEnvelope, faLock, faGithub);
-Vue.use(VueResource, PasswordHash, VueI18n);
+
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
+
+
 new Vue({
     router,
     store,
+    i18n,
     render: h => h(App),
 }).$mount('#app');
 
+
+const messages = {
+    en: {
+      message: {
+        hello: 'hello world'
+      }
+    },
+    ja: {
+      message: {
+        hello: 'こんにちは、世界'
+      }
+    }
+  }
+  
+  // Create VueI18n instance with options
+  const i18n = new VueI18n({
+    locale: 'ja', // set locale
+    messages, // set locale messages
+  })
 
 
 
