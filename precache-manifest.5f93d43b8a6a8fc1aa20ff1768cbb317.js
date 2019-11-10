0ffcf21c49f370b32a200d1dff624c26affd2151
mac <mac@MacBook-Pro-mac.local>
2019-11-10 19:29:45 +0100

    Initial dist subtree commit

---
+++ precache-manifest.5f93d43b8a6a8fc1aa20ff1768cbb317.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "23bbd8c811270fe79fe3",
+    "url": "/css/app.b0fe5ccb.css"
+  },
+  {
+    "revision": "23bbd8c811270fe79fe3",
+    "url": "/js/app.33980f00.js"
+  },
+  {
+    "revision": "996e93acd4234ef58ce2",
+    "url": "/js/chunk-vendors.f9fa63f8.js"
+  },
+  {
+    "revision": "41be261b89c09373df27e95d5ec97a78",
+    "url": "/img/logo.41be261b.png"
+  },
+  {
+    "revision": "1cf4d854709851bd2cd3f98d81f9a154",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
