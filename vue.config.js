db31e0da3ba148612e7d85ee5710179ffce39323
mac <mac@MacBook-Pro-mac.local>
2019-11-10 19:34:39 +0100

    sync

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: true,
+    publicPath: 'pwo_calendar',
+    pluginOptions: {
+        i18n: {
+            locale: 'en',
+            fallbackLocale: 'en',
+            localeDir: 'locales',
+            enableInSFC: true,
+        },
     },
-  },
-  lintOnSave: true,
-  transpileDependencies: [
-    'vuetify',
-  ],
+    lintOnSave: true,
+    transpileDependencies: [
+        'vuetify',
+    ],
 };
